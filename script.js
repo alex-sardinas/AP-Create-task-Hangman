a6c42db2d5d1ec41ab980f58bd36de5bb183de47
Alexander Sardinas <21345@columbushs.com>
2020-02-14 10:23:37 -0500

    nada done

---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById('let-play-button')
+
+
+button.onclick = function(){
+    let typing = document.getElementById('typing')
+    if (typing.style.display === 'none'){
+        typing.style.display = 'block'
+    } else {
+        typing.style.display = 'block'
+    }
+}
+
