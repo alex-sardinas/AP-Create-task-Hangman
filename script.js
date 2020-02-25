448768158cc5fb993af04af9439260c086c8a5b7
Alex-Sardinas <asardinas21345@columbushs.com>
2020-02-24 19:21:08 -0500

    hellooo

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+let wordDiv = document.getElementById('word')
+let hangmanParts = document.querySelectorAll('.hangman-part');
+let wrong = document.getElementById('wrong-letters');
 let button = document.getElementById('let-play');
-let words = ['Godfather', 'Goodfellas', 'Seven', 'Jumanji', 'Ratatouille']
+let winningScreen = document.getElementById('winning-screen')
+let losingScreen = document.getElementById('losing-screen')
 
+let words = ['godfather', 'goodfellas', 'seven', 'jumanji', 'ratatouille'];
+let theWord = words[Math.floor(Math.random() * words.length)];
+
+let haha = 'boi';
+
+let correctLetters = ['g'];
+let wrongLetters = [];
+
+function guessing() {
+    wordDiv.innerHTML =
+    `
+    ${theWord
+        .split('')
+        .map(
+            letter => `
+                <span class="l">
+                    ${correctLetters.includes(letter) ? letter : ''} 
+                </span>
+            `
+        )
+        .join('')}
+    `
+    
+}
+
+
+function wrongLettersFun() {
+
+}
+
+guessing();
+console.log(wordDiv)
+console.log(theWord)
